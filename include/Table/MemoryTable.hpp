18145d37a506c021cfa2349ff388912296bc500b
Chang Yao Chung <yaochung9744@gmail.com>
2021-12-19 14:15:34 -0800

    define MemoryTable

--- include/Table/MemoryTable.hpp
+++ include/Table/MemoryTable.hpp
@@ -2,10 +2,13 @@
 #define MemoryTable_GUARD
 
 #include <Table/Table.hpp>
+#include <unordered_set>
 
 class MemoryTable : public Table{
+private:
+    std::unordered_set<RowType> rows;
 public:
-    void insert(RowType);
+    void insert(RowType row);
     std::size_t remove(std::function<bool(RowType)> filter);
     std::size_t cardinality();
     std::size_t degree();
