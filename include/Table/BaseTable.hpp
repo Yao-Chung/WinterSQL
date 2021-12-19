6451bb19ea8d5495c8efe46bf29338a16a4b60e2
Chang Yao Chung <yaochung9744@gmail.com>
2021-12-18 23:27:09 -0800

    define misc and BaseTable

---
+++ include/Table/BaseTable.hpp
@@ -0,0 +1,9 @@
+#ifndef BaseTable_GUARD
+#define BaseTable_GUARD
+
+#include <misc.hpp>
+#include <Table/Table.hpp>
+
+using BaseTable = Site<Table>;
+
+#endif
---
+++ include/misc.hpp
@@ -0,0 +1,8 @@
+#ifndef MISC_GUARD
+#define MISC_GUARD
+
+#include <optional>
+
+using Site = std::optional;
+
+#endif
