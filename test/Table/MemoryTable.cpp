087de7676ebf1b8b9bd38879f6bc822eabd7021e
LuisHsu <luishsuplus@gmail.com>
2021-12-19 15:45:08 -0800

    Update MemoryTable begin() & end() tests

--- test/Table/MemoryTable.cpp
+++ test/Table/MemoryTable.cpp
@@ -16,3 +16,16 @@ TEST(MemoryTable, degree){
     MockMemoryTable table;
     EXPECT_EQ(table.degree(), 3);
 }
+
+TEST(MemoryTable, begin){
+    MockMemoryTable table;
+    EXPECT_EQ((*table.begin())["name"].type(), typeid(const char*));
+    EXPECT_STREQ(std::any_cast<const char*>((*table.begin())["name"]), "test");
+    EXPECT_EQ(std::any_cast<int>((*table.begin())["age"]), 20);
+    EXPECT_EQ(std::any_cast<double>((*table.begin())["weight"]), 50.8);
+}
+
+TEST(MemoryTable, end){
+    MockMemoryTable table;
+    EXPECT_EQ(table.end(), ++table.begin());
+}
