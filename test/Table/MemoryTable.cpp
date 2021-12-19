1cffb9c1d4733e89acf1dd7d31f161b52c0ed18e
Chang Yao Chung <yaochung9744@gmail.com>
2021-12-19 15:44:33 -0800

    update unittest

--- test/Table/MemoryTable.cpp
+++ test/Table/MemoryTable.cpp
@@ -10,9 +10,37 @@ struct MockMemoryTable: public MemoryTable{
             {"weight", 50.8},
         });
     }
+    std::list<RowType> &getRows(){
+        return rows;
+    }
 };
 
 TEST(MemoryTable, degree){
     MockMemoryTable table;
     EXPECT_EQ(table.degree(), 3);
 }
+
+TEST(MemoryTable, cardinality){
+    MockMemoryTable table;
+    EXPECT_EQ(table.cardinality(), 1);
+}
+
+TEST(MemoryTable, insert){
+    MockMemoryTable table;
+    std::list<RowType> &rows = table.getRows();
+    table.insert({
+        {"name", "HAHA"},
+        {"age", 28},
+        {"weight", 78.8},
+    });
+    EXPECT_EQ(rows.size(), 2);
+}
+
+TEST(MemoryTable, removeAll){
+    MockMemoryTable table;
+    std::size_t remove_number = table.remove([](RowType) -> bool {
+        return true;
+    });
+    EXPECT_EQ(remove_number, 1);
+}
+
